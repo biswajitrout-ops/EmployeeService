02f82e73b06a82054e55215e5c45f1e2b0120245
biswajit.rout <biswajit.rout@KDC1-L-F1BK65G.dir.svc.accenture.com>
2020-03-17 07:55:43 +0530

    Maven pom.xml updated

---
+++ src/test/java/com/biswajit/usecase/employee/EmployeeControllerTest.java
@@ -0,0 +1,17 @@
+package com.biswajit.usecase.employee;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT) 
+public class EmployeeControllerTest {
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+}
